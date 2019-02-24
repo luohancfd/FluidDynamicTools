facfa496684f767e5c355b8c1840006aab5b0d20
luohancfd <6806720+luohancfd@users.noreply.github.com>
2019-02-23 21:11:32 -0800

    Improve ReadChem.py

--- Thermo_Chemical_Properties/Chemkin/ReadThermo.py
+++ Thermo_Chemical_Properties/Chemkin/ReadThermo.py
@@ -2,18 +2,14 @@
 # encoding: utf-8
 # Read thermo.dat in Chemkin format
 # Written by https://github.com/luohancfd
+# pylint: disable
 import warnings
 import re
-from periodic import GetMass, GetAtomicNumber
-from collections import OrderedDict
 import os
+import argparse
+from collections import OrderedDict
+from periodic import GetMass, GetAtomicNumber
 
-class ChemkinError(Exception):
-    """
-    An exception class for exceptional behavior involving Chemkin files. Pass a
-    string describing the circumstances that caused the exceptional behavior.
-    """
-    pass
 
 def ReadThermoEntry(lines, H0=None, ref=None, S0=None, CAS=None):
     """
@@ -24,14 +20,14 @@ def ReadThermoEntry(lines, H0=None, ref=None, S0=None, CAS=None):
     species = lines[0][0:16].strip()
     refcode = lines[0][18:24].strip()
     formula0 = {}
-    for i in range(24,40,5):
+    for i in range(24, 40, 5):
         element,count = lines[0][i:i+2].strip(), lines[0][i+2:i+5].strip()
         if element:
             try:
-                formula0[element]=int(count)
+                formula0[element] = int(count)
             except ValueError:
                 # Chemkin allows float values for the number of atoms, so try this next.
-                formula0[element]=int(float(count))
+                formula0[element] = int(float(count))
     formula = OrderedDict(sorted(list(formula0.items()), key=lambda x: GetAtomicNumber(x[0])))
     mass = GetMass(formula)
     phase = lines[0][44]
@@ -39,26 +35,24 @@ def ReadThermoEntry(lines, H0=None, ref=None, S0=None, CAS=None):
         warnings.warn('Specie %s is in %s phase' % (species, phase))
     # Extract the NASA polynomial coefficients
     # Remember that the high-T polynomial comes first!
-    try:
-        Tmin = float(lines[0][45:55].strip())
-        Tmax = float(lines[0][55:65].strip())
-        Tint = float(lines[0][65:75].strip())
-        coeffHigh = [float(lines[1][i*15:(i+1)*15]) for i in range(5)]
-        coeffHigh += [float(lines[2][i*15:(i+1)*15]) for i in range(2)]
-        coeffLow = [float(lines[2][i*15:(i+1)*15]) for i in range(2,5)]
-        coeffLow += [float(lines[3][i*15:(i+1)*15]) for i in range(4)]
-    except (IndexError, ValueError):
-        raise ChemkinError('Error while reading thermo entry for species {0}'.format(species))
+    Tmin = float(lines[0][45:55].strip())
+    Tmax = float(lines[0][55:65].strip())
+    Tint = float(lines[0][65:75].strip())
+    coeffHigh = [float(lines[1][i*15:(i+1)*15]) for i in range(5)]
+    coeffHigh += [float(lines[2][i*15:(i+1)*15]) for i in range(2)]
+    coeffLow = [float(lines[2][i*15:(i+1)*15]) for i in range(2,5)]
+    coeffLow += [float(lines[3][i*15:(i+1)*15]) for i in range(4)]
 
-    data = {'sp':   species,
-            'nT':   2,
-            'refcode':  refcode,
-            'formula': formula,
-            'phase':    phase,
-            'm0':     mass,
-            'Trange': [[Tmin, Tint], [Tint, Tmax]],
-            'Tcommon': Tint,
-            'coeff': [coeffLow, coeffHigh]
+    data = {
+        'sp': species,
+        'nT': 2,
+        'refcode': refcode,
+        'formula': formula,
+        'phase': phase,
+        'm0': mass,
+        'Trange': [[Tmin, Tint], [Tint, Tmax]],
+        'Tcommon': Tint,
+        'coeff': [coeffLow, coeffHigh]
     }
 
     if H0:
@@ -72,15 +66,30 @@ def ReadThermoEntry(lines, H0=None, ref=None, S0=None, CAS=None):
 
     return data
 
+
 def ReadThermoBlock(content):
     """
     Read THERMO block of Chemkin
 
-    content: a list of str, it should start with 'THERMO' and end with 'END'
+    Arguments:
+        content: a list of str representing the content of file. content[0] should
+                 be "THERMO" or "THERMO ALL". content[-1] should be "END"
 
+    Return:
+        data: a dict representing the information like the following
+                Trange: [300.000, 1000.000, 5000.000]  // This is the temperature range
+                (CH2O)3:
+                    nT: 2
+                    refcode: '70590'
+                    formula: {H: 6, C: 3, O: 3}
+                    phase: G
+                    m0: 90.078
+        Trange: Temperature ranges for 2 sets of coefficients: lowest T, common T, and highest T
+                This one only exists if
+                - content is from a pure thermo file
+                - content is from gas phase or surf inp and content[0] == 'THERMO ALL'
     """
     c = content
-    n = len(content)
     i = 0
     regCommentLine = re.compile('^!')
     regEndLine = re.compile('^END')
@@ -88,14 +97,22 @@ def ReadThermoBlock(content):
     while regCommentLine.match(c[i]):
         i += 1
     if not re.match('^THERMO', c[i]):
-        raise ChemkinError('This is not THERMO Block')
+        raise ValueError('This is not THERMO Block')
     else:
         i += 1
-        Trange = [float(i) for i in c[i].split()]
-    print(Trange)
+        while regCommentLine.match(c[i]):
+            i += 1
+        w = re.findall('^\s*(\d+(?:\.\d*)?|\.\d+)\s*(\d+(?:\.\d*)?|\.\d+)\s*(\d+(?:\.\d*)?|\.\d+)\s*$',c[i])
+        if w:
+            Trange = [float(i) for i in w[0]]
+            i += 1
+            while regCommentLine.match(c[i]):
+                i += 1
+        else:
+            Trange = [300.000, 1000.000, 5000.000]  # default value
+            i -= 1  # i is now data line
 
     # Loop all data block
-    i += 1
     nsp = 0
     regRef = re.compile('Source:(.*)$')
     regH0 = re.compile('H0\(298K\)\s*=\s*([+-]?\d+\.?\d*)')
@@ -103,7 +120,7 @@ def ReadThermoBlock(content):
     regCAS = re.compile('CAS="(.*)"')
     regC = [regRef, regH0, regS0, regCAS]
 
-    data = []
+    data = dict()
     while not regEndLine.match(c[i]):
         commentBlock = []
         dataBlock = []
@@ -117,7 +134,6 @@ def ReadThermoBlock(content):
             i += 1
             if regEndLine.match(c[i]):
                 break
-
 
         # extract information from comment lines
         auxopt = [None for i in regC]
@@ -134,14 +150,16 @@ def ReadThermoBlock(content):
 
         # process dataBlock
         dat = ReadThermoEntry(dataBlock, ref=auxopt[0], H0=auxopt[1], S0=auxopt[2], CAS=auxopt[3])
-        print('Specie: %s' % (dat['sp']))
+        # print('Specie: %s' % (dat['sp']))
         nsp += 1
-        data.append(dat)
 
-    return data
+        spName = dat.pop('sp', None)
+        data[spName] = dat
+
+    return data, Trange
 
 
-def ReadThermoData(filepath):
+def ReadThermoFile(filepath):
     '''
     Read yaml or json format thermo data
 
@@ -164,47 +182,59 @@ def ReadThermoData(filepath):
         ]
     '''
     fname, fext = os.path.splitext(filepath)
-    with open(filepath, 'r', encoding='utf-8') as f:
-        if 'yaml' in fext or 'yml' in fext:
-            import oyaml as yaml
-            thermo = yaml.load(f)
-        elif 'json' in fext:
-            import json
-            thermo = json.load(f)
 
-    if isinstance(thermo, list):
-        # old format
-        spDatabase = {i['sp']:i for i in thermo}
-        for i in spDatabase.values():
-            i.pop('sp', None)
+    if 'inp' in fext or 'dat' in fext:
+        with open(filepath, 'r', encoding='utf-8') as f:
+            content = f.readlines()
+        data, Trange = ReadThermoBlock(content)
     else:
-        spDatabase = thermo
-
-    return spDatabase
+        with open(filepath, 'r', encoding='utf-8') as f:
+            if 'yaml' in fext or 'yml' in fext:
+                import oyaml as yaml
+                thermo = yaml.load(f)
+            elif 'json' in fext:
+                import json
+                thermo = json.load(f)
+            else:
+                raise TypeError('Unknown file type: %s' % (filepath))
+        if isinstance(thermo, list):
+            # old format
+            data = {i['sp']:i for i in thermo}
+            for i in data.values():
+                i.pop('sp', None)
+        else:
+            data = thermo
+        Trange = data.pop('Trange', None)
+    return data, Trange
 
 if __name__ == "__main__":
-    with open('database\\therm.dat', 'r', encoding='utf-8') as f:
-        content = f.readlines()
-    data = ReadThermoBlock(content)
 
-    iWriteJson = True
-    if iWriteJson:
+    parser = argparse.ArgumentParser(description="Reader for Chemkin Thermo file")
+
+    parser.add_argument('-i', '--input', default='database\\therm.dat', help="Path of thermo input file")
+    parser.add_argument('-y', '--yaml', default='database\\therm.yaml', help="Path of yaml output file")
+    parser.add_argument('-j', '--json', default='database\\therm.json', help="Path of json output file")
+
+    ARGS = parser.parse_args()
+
+    data, Trange = ReadThermoFile(ARGS.input)
+
+    if ARGS.json:
+        # in json, we use old format
         import json
-        with open('database\\therm.json', 'w', encoding='utf-8') as f:
-            json.dump(data, f, indent=2)
+        jsonData = [{'Trange': Trange}]
+        for key,value in data.items():
+            jsonData.append({**{"sp":key}, **value})
+        with open(ARGS.json, 'w', encoding='utf-8') as f:
+            json.dump(jsonData, f, indent=2)
         import sys
-        import os
         path2 = '\\'.join(os.getcwd().split('\\')[0:-1] + ['Lewis'])
         sys.path.append(path2)
         from loadthermo import PrettyPrint
         z = PrettyPrint(os.path.join(os.getcwd(), 'database\\therm.json'), True)
 
-    iWriteYAML = True
-    if iWriteYAML:
+    if ARGS.yaml:
         import oyaml as yaml
-        yamlData = {i['sp']: i for i in data}
-        for i in yamlData.values():
-            i.pop('sp')
-            #i['formula'] = dict(i['formula'])
-        with open('database\\therm.yaml', 'w', encoding='utf-8') as f:
+        yamlData = {**{'Trange': Trange}, **data}
+        with open(ARGS.yaml, 'w', encoding='utf-8') as f:
             yaml.dump(yamlData, f, encoding='utf-8')
